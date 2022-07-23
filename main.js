746121d9130cc314745ddc121bb7d95c9bced8db
Camilli Joannes <joannescamilli1@gmail.com>
2022-07-22 23:56:56 -0300

    Uploading all the project. The Escope is ready.

---
+++ main.js
@@ -0,0 +1,146 @@
+var elemento = document.getElementById("homemaranha")
+elemento.addEventListener('click', mudardisplay);
+function mudardisplay(){
+    document.getElementById("modal-homemaranha").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+var batman = document.getElementById("batmam")
+batman.addEventListener('click', mudardisplaybatman);
+function mudardisplaybatman(){
+    document.getElementById("modal-batman").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+var eternos = document.getElementById("eternos")
+eternos.addEventListener('click', mudardisplayeternos);
+function mudardisplayeternos(){
+    document.getElementById("modal-eternos").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+var encanto = document.getElementById("encanto")
+encanto.addEventListener('click', mudardisplayencanto);
+function mudardisplayencanto(){
+    document.getElementById("modal-encanto").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+var red = document.getElementById("red")
+red.addEventListener('click', mudardisplayred);
+function mudardisplayred(){
+    document.getElementById("modal-red").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+var lucifer = document.getElementById("lucifer")
+lucifer.addEventListener('click', mudardisplaylucifer);
+function mudardisplaylucifer(){
+    document.getElementById("modal-lucifer").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+var euphoria = document.getElementById("euphoria")
+euphoria.addEventListener('click', mudardisplayeuphoria);
+function mudardisplayeuphoria(){
+    document.getElementById("modal-euphoria").style.display = "block";
+    document.getElementById("transparencia").style.display = "block";
+}
+
+
+var fechar = document.getElementById("fechar")
+fechar.addEventListener('click', fecharmodal);
+function fecharmodal(){
+    document.getElementById("modal-homemaranha").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+var fecharbatman = document.getElementById("fechar2")
+fecharbatman.addEventListener('click', fecharmodalbatman);
+function fecharmodalbatman(){
+    document.getElementById("modal-batman").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+var fechareternos = document.getElementById("fechar3")
+fechareternos.addEventListener('click', fecharmodaleternos);
+function fecharmodaleternos(){
+    document.getElementById("modal-eternos").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+var fecharencanto = document.getElementById("fechar4")
+fecharencanto.addEventListener('click', fecharmodalencanto);
+function fecharmodalencanto(){
+    document.getElementById("modal-encanto").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+var fecharred = document.getElementById("fechar5")
+fecharred.addEventListener('click', fecharmodalred);
+function fecharmodalred(){
+    document.getElementById("modal-red").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+var fecharlucifer = document.getElementById("fechar6")
+fecharlucifer.addEventListener('click', fecharmodallucifer);
+function fecharmodallucifer(){
+    document.getElementById("modal-lucifer").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+var fechareuphoria = document.getElementById("fechar7")
+fechareuphoria.addEventListener('click', fecharmodaleuphoria);
+function fecharmodaleuphoria(){
+    document.getElementById("modal-euphoria").style.display = "none";
+    document.getElementById("transparencia").style.display = "none";
+}
+
+function lista(){
+    document.getElementById('lista').style.backgroundColor = 'green'
+}
+function listabatman(){
+    document.getElementById('listabatman').style.backgroundColor = 'green'
+}
+function listaeternos(){
+    document.getElementById('listaeternos').style.backgroundColor = 'green'
+}
+function listaencanto(){
+    document.getElementById('listaencanto').style.backgroundColor = 'green'
+}
+function listared(){
+    document.getElementById('listared').style.backgroundColor = 'green'
+}
+function listalucifer(){
+    document.getElementById('listalucifer').style.backgroundColor = 'green'
+}
+function listaeuphoria(){
+    document.getElementById('listaeuphoria').style.backgroundColor = 'green'
+}
+
+
+
+function gostar(){
+    document.getElementById('gostar').style.backgroundColor = 'green'  
+}
+function gostarbatman(){
+    document.getElementById('gostarbatman').style.backgroundColor = 'green'
+    
+}
+function gostareternos(){
+    document.getElementById('gostareternos').style.backgroundColor = 'green'
+    
+}
+function gostarencanto(){
+    document.getElementById('gostarencanto').style.backgroundColor = 'green'
+}
+function gostarred(){
+    document.getElementById('gostarred').style.backgroundColor = 'green'
+}
+function gostarlucifer(){
+    document.getElementById('gostarlucifer').style.backgroundColor = 'green'
+}
+function gostareuphoria(){
+    document.getElementById('gostareuphoria').style.backgroundColor = 'green'
+}
